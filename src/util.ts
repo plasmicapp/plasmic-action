6db285cae643c98adc44d797d113019ca77ab476
Chung Wu <chungwu@plasmic.app>
2023-04-11 18:31:27 -0700

    [git] Detect yarn2 vs yarn, and use different `add` commands
    Change-Id: I404a12f6430bc3b0a2fd627c0a3cb6c831b04a73

--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,8 @@ import * as core from "@actions/core";
 import { nanoid } from "nanoid";
 import { existsSync } from "fs";
 import path from "path";
+import semver from "semver";
+import { execSync } from "child_process";
 
 export type Outputs = {
   synced: boolean;
@@ -31,10 +33,12 @@ export function mkPackageManagerCmds(cwd: string): PackageManagerCmds {
     existsSync(path.join(".", "yarn.lock"));
 
   if (useYarn) {
+    const yarnVersion = execSync(`yarn --version`).toString().trim();
+    const is2 = semver.gte(yarnVersion, "2.0.0");
     return {
       install: "yarn",
       run: "yarn",
-      add: "yarn add -W",
+      add: is2 ? "yarn add" : "yarn add -W",
       cmd: "yarn",
     };
   }
